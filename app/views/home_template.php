39468ff119440c84eb21dc88be4af2f8afaf7651
unknown <gabrielsenasaraiva7@gmail.com>
2025-07-10 12:08:38 -0300

    style localhost path

--- app/views/home_template.php
+++ app/views/home_template.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $title; ?></title>
-    <link rel="stylesheet" href="/styles/style2.css">
+    <link rel="stylesheet" href="/adot/public/styles/style2.css">
     <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
     <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
     <link rel="shortcut icon" href="/favicon.ico" />
